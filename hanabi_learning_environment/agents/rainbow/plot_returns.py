74343598ed1939499504c450a5f790ae55857fe1
Victor Wang <vwang@cs.utexas.edu>
2024-03-27 15:58:42 -0500

    plot_returns.py

--- hanabi_learning_environment/agents/rainbow/plot_returns.py
+++ hanabi_learning_environment/agents/rainbow/plot_returns.py
@@ -2,6 +2,9 @@
 read `Average per episode return` logs from rainbow.out
 """
 
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 with open('rainbow.out') as f:
     returns = []
@@ -9,3 +12,8 @@ with open('rainbow.out') as f:
         if 'tensorflow:Average per episode return' in line:
             returns.append(float(line.split()[-1]))
     print(returns)
+
+# plot returns
+plt.plot(returns)
+# save plot
+plt.savefig('returns.png')
