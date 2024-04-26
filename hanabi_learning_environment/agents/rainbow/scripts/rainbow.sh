e6e8e0d520a4a839072cec9720f0d568993cabc7
Victor Wang <vwang@cs.utexas.edu>
2024-04-26 18:51:10 -0500

    rainbow.sh

--- hanabi_learning_environment/agents/rainbow/scripts/rainbow.sh
+++ hanabi_learning_environment/agents/rainbow/scripts/rainbow.sh
@@ -1,7 +1,7 @@
 mkdir -p rainbow_results
 python train.py --base_dir=results_base --mode=base &> rainbow_results/base.txt
 python train.py --base_dir=results_cheat --mode=cheat &> rainbow_results/cheat.txt
-python train.py --base_dir=tom0 --mode=tom0 &> rainbow_results/tom0.txt
-python train.py --base_dir=tom1 --mode=tom1 &> rainbow_results/tom1.txt
+python train.py --base_dir=results_tom0 --mode=tom0 &> rainbow_results/tom0.txt
+python train.py --base_dir=results_tom1 --mode=tom1 &> rainbow_results/tom1.txt
 
 python plot_returns.py
